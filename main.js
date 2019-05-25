8cbc94b204349dc5f9678a96e690a4352f19f783
TabloidA <aydanartist@gmail.com>
2019-05-25 16:07:57 -0400

    1.0.2 Code Update

--- main.js
+++ main.js
@@ -4,14 +4,12 @@ const obs = new OBSWebSocket();
 const { app, BrowserWindow } = require('electron');
 // const player = require('play-sound')(opts = {player: "C:/Users/aydan/Downloads/mpg123-1.25.10-x86-64/mpg123.exe"});
 // const player = require('play-sound')(opts = {player: "C:/Users/aydan/Downloads/mplayer-svn-38117/mplayer.exe"});
-const player = require('play-sound')(opts = {});
-const mp3Duration = require('mp3-duration');
+const player = require('play-sound')(opts = {player: "mpg123.exe"});
 var espeak = require('espeak');
-var wavFileInfo = require('wav-file-info');
-const { getAudioDurationInSeconds } = require('get-audio-duration');
 var exec = require('executive');
 
 const fs = require('fs');
+const request = require('request')
 
 const Store = require('electron-store');
 const store = new Store();
@@ -19,10 +17,18 @@ var config = {}
 var default_config = {
     "robotId": "101",
     "audioPath": "",
+    "toggleYT": false,
     "commands": [],
-    "chatToken": "xxx"
+    "chatToken": "xxx",
+    "rs": {
+        "api_host": "api.robotstreamer.com",
+        "api_port": "8080",
+        "api_protocol": "http",
+        "chat_host": "robotstreamer.com",
+        "chat_port": "8765",
+        "chat_protocol": "ws",
+    }
 }
-// store.set('config',null)
 if (!store.get('config')) {
     console.log('loading default settings')
     store.set('config', default_config)
@@ -34,7 +40,6 @@ if (!store.get('config')) {
 }
 var audioPath = store.get('config.audioPath');
 
-const say = require('say');
 const {ipcMain} = require('electron');
 
 const prefix = "!";
@@ -74,6 +79,7 @@ ipcMain.on('addCmd', (event, arg) => {
     title: arg.title,
     command: arg.action,
     detail: arg.value,
+    isEnabled: true,
     id: arg.id
   })
   console.log(cmds);
@@ -109,8 +115,86 @@ ipcMain.on('updatePath', (event, arg) => {
   arg = arg.replace(/\\/g,"/") + '/';
   store.set('config.audioPath', arg)
 })
+ipcMain.on('updateYT', (event, arg) => {
+  store.set('config.toggleYT', arg)
+})
+ipcMain.on('resetConfig', (event, arg) => {
+  store.set('config',null)
+})
 
-var ws = new WebSocket("ws://45_63_68_108.robotstreamer.com:8769/");
+var wsChat = null;
+
+function initChat(){
+  console.log('Init Chat..')
+  getChatEndpoint(store.get('config.rs.api_protocol'), store.get('config.rs.api_host'), store.get('config.rs.api_port'), true)
+}
+initChat()
+
+function getChatEndpoint(api_protocol, api_host, api_port, logs){
+    // http://api.robotstreamer.com:8080/v1/get_endpoint/jsmpeg_video_capture/211
+    var url = api_protocol + '://' + api_host + ':' + api_port + '/v1/get_endpoint/rschat/100'
+	if (logs) console.log("chat endpoint url: ", url)
+	var options = {
+		url: url,
+		method: 'GET',
+		headers: {
+			"content-type": "application/json",
+		}
+	}
+	if (logs) console.log(options)
+	request(options, function(error, response, body) {
+		if (logs) console.log('getChatEndpoint error:', error);
+		if (logs) console.log('getChatEndpoint statusCode:', response && response.statusCode);
+		if (logs) console.log('getChatEndpoint body:', body);
+        if (!error && body){
+            body = JSON.parse(body)
+            store.set('config.rs.chat_host',body.host)
+            store.set('config.rs.chat_port',body.port)
+            openChatHostWebsocket()
+        }
+	})
+}
+
+function openChatHostWebsocket() {
+    var host = store.get('config.rs.chat_host') || "robotstreamer.com"
+    var port = store.get('config.rs.chat_port') || 8765
+    var protocol = store.get('config.rs.chat_protocol') || "http"
+	console.log('<== ws-chat opening.. '.yellow, host, port);
+    if(wsChat) {
+        wsChat.close()
+        wsChat = null
+    }
+	wsChat = new WebSocket(protocol + '://' + host + ':' + port + '/echo');
+
+	wsChat.on('open', function open() {
+		console.log('<=> ws-chat open '.yellow, host, port);
+		var command = '{"message": "message"}'
+		console.log('sending command: ', command)
+		wsChat.send(command)
+	});
+
+	wsChat.on('message', function incoming(data) {
+        let obj = JSON.parse(data);
+        if (obj.robot_id == store.get('config.robotId')) {
+          checkMessageNew(obj);
+          // console.log(obj);
+        }
+	});
+
+    wsChat.on('error', function(error){
+        console.log(error)
+    })
+
+    wsChat.on('close', function(error){
+        setTimeout(function(){
+    		console.log('chat client closed connection'.red, error)
+            if (store.get('config.rs.tts_enabled')){
+              initChat()
+            }
+    	},1000)
+    })
+}
+
 
 // OBS STUFF
 obs.connect({
@@ -134,20 +218,7 @@ obs.on('error', err => {
     console.error('socket error:', err);
 });
 
-// CHAT STUFF
-ws.onopen = function (event) {
-  ws.send(JSON.stringify({type: 'connect', message: 'joined'}));
-};
 
-ws.onmessage = function (event) {
-  //console.log("ws client received", event.data);
-  //console.log("event.data", event.data);
-  let obj = JSON.parse(event.data);
-  if (obj.robot_id == store.get('config.robotId')) {
-    checkMessageNew(obj);
-    // console.log(obj);
-  }
-}
 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -172,7 +243,7 @@ var loopArray = function(arr) {
 function waitForAction(msg,arr,callback) {
       arr.shift();
       if (!doCommand(msg,callback)) {
-        exec.quiet('espeak "'+msg+'"', function() {
+        exec.quiet('espeak "'+msg.substr(0,75)+'"', function() {
           console.log("end of file");
           callback();
         });
@@ -182,8 +253,25 @@ function waitForAction(msg,arr,callback) {
 var checkMessageNew = function(msg) {
   if (msg.tts === false) { return; }
 
-  messagesToTTS.push(msg.message);
+  let isYtToggled = store.get('config.toggleYT');
+  if (msg.message.includes(prefix+'yt') && isYtToggled) {
+    var args = msg.message.split(' ');
+    var key = args[1].replace('https://www.youtube.com/watch?v=','');
+    var time = args[2];
+    if (!time) {
+      time = "0";
+    }
 
+    obs.send('SetBrowserSourceProperties', {
+      'source': "yt",
+      'url': "https://www.youtube.com/embed/"+key+"?autoplay=1&start="+time
+    }).catch(err => {
+        console.log(err);
+    });
+    return;
+  }
+
+  messagesToTTS.push(msg.message);
   if (!soundPlaying) {
     loopArray(messagesToTTS);
     soundPlaying = true;
@@ -203,12 +291,17 @@ function doCommand(msg, callback) {
           'scene-name': cmds[i].detail
         });
       } else if(cmds[i].command == 'playAudio') {
-        console.log("is audio command");
-        output = true;
-        player.play(store.get('config.audioPath')+cmds[i].detail, { mplayer: ['-v', 0.2 ] }, function(err) {
-          if (err) throw err
-          callback();
-        });
+        if (!cmds[i].isEnabled) {
+          output = false;
+          return output;
+        } else {
+          console.log("is audio command");
+          output = true;
+          player.play(store.get('config.audioPath')+cmds[i].detail, { mplayer: ['-v', 0.2 ] }, function(err) {
+            if (err) throw err
+            callback();
+          });
+        }
       }
     }
   }
